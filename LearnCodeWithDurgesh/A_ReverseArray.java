a7adaa9de4c7b0e8ec4679ed804c2e4570b61abc
Guddu Kumar <guddu748081@gmail.com>
2025-02-26 17:34:46 +0530

    reverse an array second method

--- LearnCodeWithDurgesh/A_ReverseArray.java
+++ LearnCodeWithDurgesh/A_ReverseArray.java
@@ -1,23 +1,47 @@
 import java.util.Arrays;
 
 public class A_ReverseArray {
-    public int [] reverseArray(int[] arr){
 
-        int [] newArr = new int[arr.length];
+    // ============ method -1 ===============
 
-        for (int i = arr.length - 1, j = 0; i >= 0; i--, j++) {
-            newArr[j] = arr[i];
-        }        
+    // public int [] reverseArray(int[] arr){
+    // int [] newArr = new int[arr.length];
+    // for (int i = arr.length - 1, j = 0; i >= 0; i--, j++) {
+    // newArr[j] = arr[i];
+    // }
+    // return newArr;
+    // }
+
+
+
+    
+    // ============ method -2 ===============
+
+    public int[] reverseArray(int[] arr) {
         
-        return newArr;
+        int start = 0;
+        int end =arr.length-1;
+
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] =arr[end];
+            arr[end] = temp;
+
+            start ++;
+            end --;
+            
+        }
+        return arr;
     }
+
+    
 
     public static void main(String[] args) {
 
-        int arr[] = {1,2,3,4,6,8,2,9};
+        int arr[] = { 1, 2, 3, 4, 6, 8, 2, 9 };
 
         A_ReverseArray rs = new A_ReverseArray();
 
-       System.out.println(Arrays.toString(rs.reverseArray(arr)));
+        System.out.println(Arrays.toString(rs.reverseArray(arr)));
     }
 }
