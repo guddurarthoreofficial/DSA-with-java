25adcee1c89b66a69abe0a3cc6aec3f13fc1aea5
Guddu Kumar <guddu748081@gmail.com>
2025-03-01 00:08:33 +0530

    Remove Nth Node from End of the List

--- PW  Raghav sir/LinkedList/NthNodeFromEnd.java
+++ PW  Raghav sir/LinkedList/NthNodeFromEnd.java
@@ -4,27 +4,29 @@ public class NthNodeFromEnd {
         int data;
 
         Node next;
-        Node(int data){
-            this.data = data; 
+
+        Node(int data) {
+            this.data = data;
         }
     }
 
+    // brute force
 
-    void nthNodeFromEnd(Node head, int index){
+    void nthNodeFromEnd(Node head, int index) {
 
-        // 1 2 3 4 5 6 7 
+        // 1 2 3 4 5 6 7
 
         Node temp = head;
 
-        if(head == null){
+        if (head == null) {
             System.out.println("list is blanks");
             return;
         }
-        
+
         int length = 0;
         while (temp != null) {
-            length ++;
-            temp = temp.next;  
+            length++;
+            temp = temp.next;
         }
 
         // Check if index is out of bounds
@@ -33,24 +35,38 @@ public class NthNodeFromEnd {
             return;
         }
 
-        int m = length - index+1;  // 7 - 3+1 = 5 th  from the begning
+        int m = length - index + 1; // 7 - 3+1 = 5 th from the begning
 
         temp = head;
-        for (int i = 1; i < m; i++){
+        for (int i = 1; i < m; i++) {
             temp = temp.next;
         }
 
         System.out.println(temp.data);
-
-
     }
 
-    
+    // =============== optimized ============
+    void nthNode2(Node head, int index) {
+
+        Node slow = head;
+        Node fast = head;
+
+        for (int i = 0; i < index; i++) {
+            fast = fast.next;
+        }
+
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        System.out.println("Data is : " +slow.data);
+    }
 
     public static void main(String[] args) {
 
         NthNodeFromEnd ll = new NthNodeFromEnd();
-        
+
         Node a = new Node(100);
         Node b = new Node(200);
         Node c = new Node(300);
@@ -62,11 +78,11 @@ public class NthNodeFromEnd {
         b.next = c;
         c.next = d;
         d.next = e;
-        e.next = f; 
+        e.next = f;
 
-        ll.nthNodeFromEnd(a,3);
+        ll.nthNodeFromEnd(a, 3);
+        ll.nthNode2(a, 3);
 
-        
     }
-    
+
 }
